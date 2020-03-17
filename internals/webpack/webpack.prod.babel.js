c7260a3a6418801a83e936976205c8488179ef64
Bayu Adi Wibowo <bayu.eighted@gmail.com>
2020-03-17 14:47:50 +0700

    build change publicpath

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -89,7 +89,7 @@ module.exports = require("./webpack.base.babel")({
     // assets manipulations and do leak its manipulations to HtmlWebpackPlugin
     new OfflinePlugin({
       relativePaths: false,
-      publicPath: "/",
+      publicPath: "/docs/",
       appShell: "/",
 
       // No need to cache .htaccess. See http://mxs.is/googmp,
